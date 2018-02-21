378c4b1d29e40c3d1c50f78853895659003413f3
SYAAKIR\Ahmad Syaakir <syaakirr@gmail.com>
2018-02-21 20:00:37 +0800

    Upload example 4

---
+++ Example4/main.c
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Ahmad Syaakir
+ *
+ * Created on February 21, 2018, 7:59 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    printf("Hello World 4");
+    return (EXIT_SUCCESS);
+}
+
